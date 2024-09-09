88c8bab2c974493e9451285e308f0d1e6d7d3154
Alan <alanjameschapman@gmail.com>
2024-09-09 10:59:04 +0100

    Draw grid.

--- code/game.py
+++ code/game.py
@@ -7,5 +7,11 @@ class Game:
         self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
 
+    def draw_grid(self):
+        for x in range(0, GAME_WIDTH, TILE_SIZE):
+            pygame.draw.line(self.surface, GRAY, (x, 0), (x, GAME_HEIGHT))
+        for y in range(0, GAME_HEIGHT, TILE_SIZE):
+            pygame.draw.line(self.surface, GRAY, (0, y), (GAME_WIDTH, y))
+
     def run(self):
         self.display_surface.blit(self.surface, (PADDING, PADDING))
