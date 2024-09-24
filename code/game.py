7991efdf0aa5ac83734d7c68ca645aa2681e5f6b
Alan <alanjameschapman@gmail.com>
2024-09-24 15:20:07 +0100

    Add horizontal move functionality

--- code/game.py
+++ code/game.py
@@ -13,7 +13,8 @@ class Game(DisplayComponent):
 
         # timer
         self.timers = {
-            'vertical move': Timer(UPDATE_START_SPEED, repeated=True, func=self.move_down)
+            'vertical move': Timer(UPDATE_START_SPEED, repeated=True, func=self.move_down),
+            'horizontal move': Timer(MOVE_WAIT_TIME)
         }
         self.timers['vertical move'].activate()
 
@@ -24,9 +25,24 @@ class Game(DisplayComponent):
     def move_down(self):
         self.tetromino.move_down()
 
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if not self.timers['horizontal move'].active:
+            if keys[pygame.K_LEFT]:
+                self.tetromino.move_horizontal(-1)
+                self.timers['horizontal move'].activate()
+            if keys[pygame.K_RIGHT]:
+                self.tetromino.move_horizontal(1)
+                self.timers['horizontal move'].activate()
+            # if keys[pygame.K_DOWN]:
+            #     self.tetromino.move_down()
+            
+
     def run(self):
 
         # update
+        self.input()
         self.timer_update()
         self.sprites.update()
 
@@ -45,6 +61,10 @@ class Tetromino(pygame.sprite.Sprite):
 
         # create blocks
         self.blocks = [Block(group,pos,self.color) for pos in self.block_positions]
+
+    def move_horizontal(self, amount):
+        for block in self.blocks:
+            block.pos.x += amount
 
     def move_down(self):
         for block in self.blocks:
